ee8525b9d7430f5eb244dcb3f81f2d05746a9d13
SaketJNU <saket_au@rediffmail.com>
2021-05-10 12:37:46 +0530

    10-05-2021

---
+++ grid_app/app/src/main/java/com/example/grid_app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.grid_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText =  findViewById(R.id.editText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=editText.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+   }
+}
